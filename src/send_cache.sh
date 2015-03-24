be4190e80d73ab251ac40e801702fc37d7b9de62
sgf <sgf.dma@gmail.com>
2015-03-24 16:58:45 +0300

    send-cache. Add option for specifying maximum cache age .
    If cache file is older, than specified time, UNKNOWN would be returned.

--- src/send_cache.sh
+++ src/send_cache.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# Usage: $0 [--cache path] [plugin which cache to read]
+# Usage: $0 [--cache path] [--fresh minutes] [plugin which cache to read]
 # If both --cache option and plugin specified, option will take precedence.
 
 set -euf
@@ -9,6 +9,7 @@ readonly True=1
 readonly False=0
 readonly nl='
 '
+readonly OIFS="$IFS"
 
 # Nagios return codes.
 readonly ret_unkn=3
@@ -21,13 +22,15 @@ readonly ret_ok=0
 # All following lines are plugin stdout.
 
 cache_file=''
+fresh=0
 plugin=''
+# I rely below, that initial values are $ret_unkn and empty $res .
 ret="$ret_unkn"
 res=''
 
 usage()
 {
-    echo "Usage: $(basename $0) [--cache path] [plugin which cache to read]"
+    echo "Usage: $(basename $0) [--cache path] [--fresh minutes] [plugin which cache to read]"
 }
 
 # All options must be before non-option arguments. Use '--' to terminate
@@ -40,6 +43,10 @@ while [ $# -gt 0 ]; do
       ;;
       '--cache' )
         cache_file="$2"
+        shift 2
+      ;;
+      '--fresh' )
+        fresh="$2"
         shift 2
       ;;
       '--' )
@@ -59,16 +66,34 @@ cache_file="${cache_file:-${plugin:+$(basename "$plugin").cache}}"
 if [ -z "$cache_file" ]; then
     ret=$ret_unkn
     res="No cache file specified, $(usage)"
-else
-    # Default cache directory, if path is relative.
-    if [ "${cache_file#.}" != "$cache_file" ]; then
-        cache_file="$(pwd)/$cache_file"
-    elif [ "${cache_file#/}" = "$cache_file" ]; then
-        cache_file="/var/cache/nagios3/$cache_file"
-    fi
-    readonly cache_file
+fi
+# Default cache directory, if path is relative.
+if [ "${cache_file#.}" != "$cache_file" ]; then
+    cache_file="$(pwd)/$cache_file"
+elif [ "${cache_file#/}" = "$cache_file" ]; then
+    cache_file="/var/cache/nagios3/$cache_file"
+fi
+readonly cache_file
+IFS="$nl"
+set -- $fresh
+if [ $# -gt 1 ] || echo "$fresh" | grep -q -e '[^0-9]'; then
+    ret=$ret_unkn
+    res="Incorrect fresh interval '$fresh'."
+fi
+IFS="$OIFS"
+readonly fresh
 
-    if [ -r "$cache_file" ]; then
+# Assume, that initial result is empty. And if result is still empty, no
+# errors was found so far.
+if [ -z "$res" ]; then
+    if [ ! -r "$cache_file" ]; then
+        ret=$ret_unkn
+        res="Can't read cache '$cache_file'."
+    elif [ "$fresh" -gt 0 -a -z "$(find "$cache_file" -mmin -"$fresh")" ];
+    then
+        ret=$ret_unkn
+        res="Cache '$cache_file' is older, than '$fresh' minutes ago."
+    else
         ret="$(head -c 1 "$cache_file")"
         if [ "$ret" != "$ret_ok" \
             -a "$ret" != "$ret_warn" \
@@ -79,9 +104,6 @@ else
             ret=$ret_unkn
         fi
         res="${res:+$res, }$(tail -n '+2' "$cache_file")"
-    else
-        ret=$ret_unkn
-        res="Cache '$cache_file' is empty."
     fi
 fi
 
